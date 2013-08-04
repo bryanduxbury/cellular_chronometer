57af83bb4ec787e1f3747ca1f6abb09c7ec9d4bd
Bryan Duxbury <duxbury@squareup.com>
2013-08-03 20:48:30 -0700

    add bitvector related stuff to Pt class

--- rb/pt.rb
+++ rb/pt.rb
@@ -26,10 +26,40 @@ class Pt
   end
 
   def hash
-    x.hash + y.hash
+    [x, y].hash
   end
 
   def translate(dx,dy)
     Pt.new(x+dx,y+dy)
+  end
+  
+  def flip
+    Pt.new(y, x)
+  end
+
+  def self.pts_to_bv_rows(pts, num_rows)
+    bvs = [0] * num_rows
+    pts.each do |pt|
+      bvs[pt.y] |= (1 << pt.x)
+    end
+    bvs
+  end
+  
+  def self.bv_rows_to_pts(bv_rows)
+    pts = []
+    rownum = 0
+    bv_rows.each do |bv|
+      colnum = 0
+      until bv == 0
+        if bv & 1 == 1
+          pts << Pt.new(colnum, rownum)
+        end
+        bv = bv >> 1
+        colnum += 1
+      end
+      rownum += 1
+    end
+
+    pts
   end
 end
