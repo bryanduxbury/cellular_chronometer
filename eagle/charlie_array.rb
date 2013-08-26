49daa09148597f206c63d3eb636647ece2b0731c
bryanduxbury <bryan@rapleaf.com>
2013-08-26 08:04:08 -0700

    adjusted to fit in 100mm width

--- eagle/charlie_array.rb
+++ eagle/charlie_array.rb
@@ -1,12 +1,12 @@
-delta = (4000 - 200) / 24.0
-bottom_gutter = (1000 - (4 * delta)) / 2
+delta = (100.0 - 5) / 24.0
+bottom_gutter = (25.0 - (4 * delta)) / 2
 
 row = 0
 col = 0
 for i in 1..12
   for j in (i+1)..12
     for pair in [[i,j],[j,i]]
-      puts "move l#{pair.first}_#{pair.last} (#{100 + col * delta} #{(4 * delta + bottom_gutter) - row * delta});"
+      puts "move l#{pair.first}_#{pair.last} (#{2.5 + col * delta} #{(4 * delta + bottom_gutter) - row * delta});"
       col+= 1
       if col == 25
         col = 0
